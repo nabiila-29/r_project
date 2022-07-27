31ded5bdaca609ae6c1685492210d986001b5c35
Nabiila <79734730+nabiila-29@users.noreply.github.com>
2022-07-27 09:46:45 +0700

    Update and rename r_shiny fundamental to shiny_template.R

---
+++ shiny_template.R
@@ -0,0 +1,23 @@
+#call libraries
+library(shiny)
+
+
+
+
+# UI 
+ui <- fluidPage()
+
+
+
+
+
+# Server
+server <- function(input,output) {}
+
+
+
+
+
+
+# Combine Function
+shinyApp(ui = ui, server = server)
